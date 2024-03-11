8368af0a8f67418456999aba2825752577d4c122
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2024-03-12 08:20:22 +1000

    switched root get to returning domain filters

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -4,6 +4,7 @@ use axum::http::header;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::{debug_handler, Json, Router};
+use serde::Serialize;
 
 use crate::appstate::DynStateTrait;
 
@@ -14,6 +15,14 @@ pub fn app() -> Router<DynStateTrait> {
 // TODO: Do we add wildcards? Are subdomains obviated by TLDs?
 #[debug_handler(state = DynStateTrait)]
 pub async fn root_get(State(state): State<DynStateTrait>) -> impl IntoResponse {
+    // TODO: Should the struct be in the function, outside, or another file?
+    #[derive(Serialize)]
+    struct DomainFilter {
+        filters: Vec<String>,
+    }
+    let reply = DomainFilter {
+        filters: state.get_domains(),
+    };
     // TODO: think about the arc and whether static stuff like domains list should be arc
     (
         [(
@@ -21,6 +30,6 @@ pub async fn root_get(State(state): State<DynStateTrait>) -> impl IntoResponse {
             // TODO: again, should this match the request or will it always be this?
             "application/external.dns.webhook+json;version=1",
         )],
-        Json::from(state.get_domains()),
+        Json::from(reply),
     )
 }
