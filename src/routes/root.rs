97cfbeee16bdd0e4b51d95e28082d13c0e95c83a
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2024-03-12 08:15:33 +1000

    set content type correctly on root responses

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -1,5 +1,6 @@
 use axum::extract::State;
 
+use axum::http::header;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::{debug_handler, Json, Router};
@@ -14,5 +15,12 @@ pub fn app() -> Router<DynStateTrait> {
 #[debug_handler(state = DynStateTrait)]
 pub async fn root_get(State(state): State<DynStateTrait>) -> impl IntoResponse {
     // TODO: think about the arc and whether static stuff like domains list should be arc
-    Json::from(state.get_domains())
+    (
+        [(
+            header::CONTENT_TYPE,
+            // TODO: again, should this match the request or will it always be this?
+            "application/external.dns.webhook+json;version=1",
+        )],
+        Json::from(state.get_domains()),
+    )
 }
